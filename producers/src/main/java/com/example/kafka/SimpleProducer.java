be6ffb2aa4b15508edce85549f4b26448d579817
kdg0209 <s_cova_0418@naver.com>
2023-12-31 20:22:00 +0900

    feat: kafka producer 모듈 생성
    1. 동기방식의 producer
    2. 비동기방식의 producer

--- producers/src/main/java/com/example/kafka/SimpleProducer.java
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -1,6 +1,34 @@
 package com.example.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class SimpleProducer {
 
+    private static final String BOOTSTRAP_SERVERS_CONFIG = "localhost:9092";
+    private static final String TOPIC_NAME = "simple-topic";
 
+    public static void main(String[] args) {
+        // 1. kafkaProducer configuration setting
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 2. kafkaProducer object create
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        // 3. kafkaProducerRecord object create
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "hello-world 1");
+
+        // 4. producer message send
+        kafkaProducer.send(record);
+
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
 }
